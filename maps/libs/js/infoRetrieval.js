9db4a7269eab877ffef94989e70df01baccd85b8
LewisSwalesSD <66833329+LewisSwalesSD@users.noreply.github.com>
2020-12-02 16:57:29 +0000

    interactive map 2.1
    implemented clicklinks and country data display

--- maps/libs/js/infoRetrieval.js
+++ maps/libs/js/infoRetrieval.js
@@ -1,224 +1,89 @@
-var localData;
-var weatherData;
-var countryData;
+
 
 //function is called update local as the local info must be updated before other apis
 //this function is called when the map is clicked on. this happens at map.js in the map
 // .on("click.... function
-function update(){
+function getCountryInfo(){
     $.ajax({
-            url:'libs/php/getlocaldata.php',
-            type:'POST',
-            dataType:'json',
-            data:{
-                lat: `${lat}`,
-                lng: `${lng}`
-            },
-    
-            success: function(result){
-                localDataFunc(result);
-                
-            },
-    
-            error: function(jqXHR, textStatus,errorThrown){
-                            console.log(jqXHR, textStatus, errorThrown);
-                            console.log("localdata problem");
-                            $('#info').html("it seems there has been an error, click again please...")
-                    }
+        url:"libs/php/getcountryinfo.php",
+        type: "POST",
+        dataType: 'json',
+        data:{
+            countryName: countryName
+        },
+
+        success: function(result){
+            console.log("%cCountryData() ", "color: red");
+            console.log(result);
             
-    })
-};
-
-//only DataFunc which needs result as it returns town variable for the other api searches
-function localDataFunc(result){
-    console.log(result)
-    //storing variables for easy access later.
-    localData = result['data'][0];
-    console.log(`heres where the problem is ${result['data'][0]}`)
-    town = result['data'][0]['name'];
-
-    CountryDataFunc();
-    if(localData == undefined){
-        $('#info').append(`<p>You clicked to far away form a town. click closer next time</p>`)
-        console.log("hiya")
-    } else {
-    wikiDataFunc();
-    WeatherDataFunc();
-    refreshLocalData();
-    }
-
-    
-    
-}
-
-//functions for getting data from apis on click. All are nested in local Data as 
-//local data must go first. see above.
-function wikiDataFunc(){
-    $.ajax({
-        url:'libs/php/getwikidata.php',
-            type:'POST',
-            dataType:'json',
-            data:{
-                lat: `${lat}`,
-                lng: `${lng}`                            
-            },
-    
-            success: function(result){
-                console.log(`wikipedia: ${result}`)
-                //storing variables for easy access later.
-                wikiData = result;    
-                refreshWikiData();                        
-            },
-    
-            error: function(jqXHR, textStatus,errorThrown){
-                    console.log(jqXHR, textStatus, errorThrown)
-            }
-
-    })
-}
-
-function WeatherDataFunc(){
-    $.ajax({
-        url:'libs/php/getweatherinfo.php',
-            type:'POST',
-            dataType:'json',
-            data:{
-                town: `${town}`                           
-            },
-    
-            success: function(result){
-                console.log(`weather: ${result}`)
-                //storing variables for easy access later.
-                weatherData = result;  
-                //refreshWeatherData();                          
-            },
-    
-            error: function(jqXHR, textStatus,errorThrown){
-                    console.log(jqXHR, textStatus, errorThrown)
-                    console.log("problem with weather")
-            }
-
-    })
-}
-
-function CountryDataFunc(){
-    $.ajax({
-        url:'libs/php/getcountryinfo.php',
-            type:'POST',
-            dataType:'json',
-            data:{
-                countryName: `${countryName}`                           
-            },
-    
-            success: function(result){
-                console.log(`country: ${result}`)
-                //storing variables for easy access later.
                 countryData = result;
-                refreshCountryData(); 
-                                           
-            },
-    
-            error: function(jqXHR, textStatus,errorThrown){
-                    console.log(jqXHR, textStatus, errorThrown);
-                    console.log("problem with country")
-                    
+                countryDataRefresh();
+            
+            $('.info').addClass("hidden");
+            $('#country').removeClass("hidden");
 
-            }
-        
-                
+            //if we done get a country returned we can prompt people to try again. 
+            //mostly its because I sometimes click on sea and think the code is broken. 
+            //code have saved myself 30 or so minutes if I had caught this early on.
+            
+            $('.overlay').removeClass("hidden");
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+        }
+
     })
-       
-
 }
 
 
 
-function refreshLocalData(){
-        if(localData['population'] > 0 && weatherData['data'] != null){
-            $('#local').html(
-                `
-                <p><b>Nearest city to click:</b> ${town} in ${localData['adminName1']}</p>
-                <p><b>Population<b>: ${localData['population']}</p>
-                <p><b>Current Weather:</b> ${weatherData['data']['condition']['text']}<img src="${weatherData['data']['condition']['icon']}"></p>
-                <p><b>Wind speed:</b>${weatherData['data']['wind_mph']} mph </p>
-                <p></p>
-                `
-            )
-        } else {
-            $('#local').html(
-                `
-                <p>Name of nearest place: ${town} in ${localData['adminName1']}</p>
-                <p>population: population data unavailable</p>
-                
-        
-                `
-        
-            )
-        }
+function countryDataRefresh(){
 
-    if(weatherData['data'] == null){
-        $('#local').html(
-            `
-                <p><b>Nearest city to click:</b> ${town} in ${localData['adminName1']}</p>
-                <p><b>Population<b>: ${localData['population']}</p>
-                <p><b>Weather Data unavaliable</b></p>
-                `
-        )
-    }
-}   
+if(countryData[1] == undefined || countryData[1] != "United States of America"){
+    $('#country').html(`
+                    <p><b>lattitude:</b> ${lat.toFixed(5)}<br>
+                    <b>longitude:</b>${lng.toFixed(5)}</p>
+                    <p><b> You have selected</b>: ${countryName} </p><br>
+                    <p> ${countryName} has a population of ${countryData[0]['population']} and the total area of ${countryName} is ${countryData[0]['area']}km<sup>2</sup>which means icelands population density is about ${(countryData[0]['population']/countryData[0]['area']).toFixed(0)} People per km<sup>2</sup>.<br>
+                    The ${countryData[0]['languages'].length < 2 ? `language of ${countryName} is ` : `languages of ${countryName} are `}<span id="languages"></span> and they use the ${countryData[0]['currencies'][0]['name']}(${countryData[0]['currencies'][0]['symbol']}) as their main currency.
 
-/*function refreshWeatherData(){
-    if(weatherData.data == null){
-        $('#weather').html(
-            `weather data not availiable due to api limitations. wait a minute and try again`
-        )
-    } else{
-    $('#weather').html(
-        
-        
-    )
-    }
-}*/
 
-function refreshWikiData(){
-        $('#wiki').html(`<p>Here are some Wiki links for nearby places<p>`);
+                `)
 
-        for(var i = 0; i < wikiData['data'].length; i++){
-            $('#wiki').append(
-            `<p>${i+1}.<a href="https://${wikiData['data'][i]['wikipediaUrl']}" target="_blank">${wikiData['data'][i]['title']}</a></p>`  
-            )
-        }
-        
+                for(var i = 0; i < countryData[0]['languages'].length; i++){
+                    $('#languages').append(`${countryData[0]['languages'][i]['name']}, `)
+                }
+}    
+else if(countryData[1]['name'] == "United States of America"){
+    $('#country').html(`
+                    <p><b>lattitude:</b> ${lat.toFixed(5)}<br>
+                    <b>longitude:</b>${lng.toFixed(5)}</p>
+                    <p><b> You have selected</b>: ${countryName} </p><br>
+                    <p> ${countryName} has a population of ${countryData[1]['population']}. The total area of ${countryName} is ${countryData[1]['area']}km<sup>2</sup>which means icelands population density is about ${(countryData[1]['population']/countryData[1]['area']).toFixed(0)} People per km<sup>2</sup>.<br>
+                    the Language of ${countryName} is <span id="languages"></span> and they use the ${countryData[1]['currencies'][0]['name']}(${countryData[1]['currencies'][0]['symbol']}) as their main currency.
+
+
+                `)
+
     
-}
-
-function refreshCountryData(){
-    $('#country').html(
-        `
-        <p><b>Country:</b> ${countryData[0]['name']} (${countryData[0]['alpha2Code']})</p>
-        <p><b>Region:</b>${countryData[0]['subregion']}</p>
-        <p><b>Capital:</b> ${countryData[0]['capital']}</p>
-        <p><b>Population:</b> ${countryData[0]['population']}</p>
-        <p><b>Area:</b>${countryData[0]['area']} km<sup>2</sup></p>
-        <p><b>Main currency:</b> ${countryData[0]['currencies'][0]['name']}
-        <p><b>Bordering Country Codes:</b> ${countryData[0]['borders'].toString(', ')}</p>
-        <p><b>Languages:</b><span id="languages"></span><p>
-        <p><b>Alternate Spellings:</b><span id="altSpelling"></span></p>
-        `
-    )
-        for(var i= 0; i<countryData[0]['languages'].length; i++){
-            $('#languages').append(
-               `<span> ${countryData[0]['languages'][i]['name']},</span>`
-            )
+        for(var i = 0; i < countryData[1]['languages'].length; i++){
+            $('#languages').append(`${countryData[1]['languages'][i]['name']}`)
         }
-
-        countryData[0]['altSpellings'].forEach(alt => {
-            $('#altSpelling').append(
-                `${alt}, `)
-        });
-
-        
-        
     
-}
 
+}}
+
+function wikiDataRefresh(){
+    $('#wiki').html(`<h2>Wiki links closest to where you clicked</h2><br>`);
+
+    var i = 1;
+    wikiData['data'].forEach(element => {
+        console.log(element)
+        $('#wiki').append(`
+        ${i}. <a href='https://${element.wikipediaUrl}' target='_blank'> ${element.title}</a><br>
+        `)
+        i++
+    });
+}
+    
--- maps/libs/js/map.js
+++ maps/libs/js/map.js
@@ -4,10 +4,12 @@ var lng;
 var countryCode;
 var countryName;
 var town;
+var localData;
+var weatherData;
+var countryData;
+var wikiData;
 
-//store arrays from api calls for easy access later if needed.
-
-
+var mapSelection = "outdoors";
 
 
 //setting up map. this includes bounds and center. center will be used to focus on countries when selected and to show users location initially
@@ -15,7 +17,7 @@ var map = L.map('map',{
     center: [57,-1],
     zoom: 4,
     minZoom: 3,
-    maxZoom: 10,
+    maxZoom: 6,
     maxBounds: [
         //south west 
         [180,-180],
@@ -23,9 +25,11 @@ var map = L.map('map',{
         [-180,180]
     ]}); 
 
+    var geoJson;
+
 
 //setting up map. currently using neighboorhod from thunderforest.
-var Thunderforest_Neighbourhood = L.tileLayer('https://tile.thunderforest.com/pioneer/{z}/{x}/{y}.png?apikey=8c17817d10c0456c853764480540314f', {
+var Thunderforest = L.tileLayer(`https://tile.thunderforest.com/${mapSelection}/{z}/{x}/{y}.png?apikey=8c17817d10c0456c853764480540314f`, {
 	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', //attribution required
 	maxZoom: 22
 }).addTo(map);
@@ -35,58 +39,14 @@ var Thunderforest_Neighbourhood = L.tileLayer('https://tile.thunderforest.com/pi
 // this funtion also revels the overlay and displays the country name and lat/lng of click for user.
 // funtions retrieving information for other overlay panes will be put in external scripts for tidiness and readability.
 map.on('click', debounce(function(e){
+    console.log(e);
     var coord = e.latlng;
     lat = coord.lat;
     lng = coord.lng;
+    map.setView([lat,lng],5)
     console.log("You clicked the map at latitude: " + lat + " and longitude: " + lng);
-            
-                $.ajax({
-                url:"libs/php/getCountryCode.php",
-                type: "POST",
-                dataType: 'json',
-                data:{
-                    lat: `${lat}`,
-                    lng: `${lng}`
-                },
-
-                success: function(result){
-                    console.log(result);
-                    //storing data into variables for use by other apis
-                    countryName= result['data']['countryName'];
-                    countryCode= result['data']['countryCode'];
-                    console.log(countryName);
-                    //revealing modal overlay
-                    $('.overlay').removeClass("hidden");
-                    if(countryName != undefined){
-                        $('#info').html(`
-                            <p> you clicked: <br>
-                            lattitude: ${lat.toFixed(5)}<br>
-                            longitude:${lng.toFixed(5)}</p>
-                            <p> You have selected: ${countryName} </p>
-                        `)
-                    }
-                    $('.info').addClass("hidden");
-                    $('#info').removeClass("hidden");
-
-                    //if we done get a country returned we can prompt people to try again. 
-                    //mostly its because I sometimes click on sea and think the code is broken. 
-                    //code have saved myself 30 or so minutes if I had caught this early on.
-                    if(countryName == undefined){
-                        $('#info').html(
-                            `<p>I think you might have clicked a sea or an ocean. try again, but on land this time</p>`
-                       ) 
-                    }
-                    update();
-                },
-
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.log(jqXHR, textStatus, errorThrown);
-                }
-        
-            })
-
-            
-        
+    basicCountryData();  
+    getwikiData();      
 },250));
         
         
@@ -95,7 +55,91 @@ map.on('click', debounce(function(e){
 
 
 
+function basicCountryData(){
+    $.ajax({
+        url:"libs/php/getCountryCode.php",
+        type: "POST",
+        dataType: 'json',
+        data:{
+            lat: `${lat}`,
+            lng: `${lng}`
+        },
 
+        success: function(result){
+            console.log(result);
+            //storing data into variables for use by other apis
+            
+            countryName= result['data']['countryName'];
+            countryCode= result['data']['countryCode'];
+
+            if(result['data']['countryName'] == "South Korea"){
+                countryName = "republic of korea"
+            }
+
+            if(result['data']['countryName'] == "North Korea"){
+                countryName = "korea";
+            }
+
+            console.log("%cbasicCountryData() ", "color: red");
+            console.log(countryName, countryCode)
+            //revealing modal overlay
+            $('.overlay').removeClass("hidden");
+            if(countryName != undefined){
+                getCountryInfo();
+                
+            }
+            $('.info').addClass("hidden");
+            $('#country').removeClass("hidden");
+
+            //if we done get a country returned we can prompt people to try again. 
+            //mostly its because I sometimes click on sea and think the code is broken. 
+            //code have saved myself 30 or so minutes if I had caught this early on.
+            if(countryName == undefined){
+                $('#info').html(
+                    `<p>I think you might have clicked a sea or an ocean. try again, but on land this time</p>`
+               ) 
+            }
+            
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+        }
+
+    })
+}
+
+function getwikiData(){
+    $.ajax({
+        url:"libs/php/getwikidata.php",
+        type: "POST",
+        dataType: 'json',
+        data:{
+            lat: `${lat}`,
+            lng: `${lng}`
+        },
+
+        success: function(result){
+            console.log("%cwikiData()", "color:red")
+            wikiData = result;
+            //storing data into variables for use by other apis
+            //revealing modal overlay
+            $('.overlay').removeClass("hidden");
+            $('.info').addClass("hidden");
+            
+            wikiDataRefresh();
+            //if we done get a country returned we can prompt people to try again. 
+            //mostly its because I sometimes click on sea and think the code is broken. 
+            //code have saved myself 30 or so minutes if I had caught this early on.
+                        
+        },
+
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR, textStatus, errorThrown);
+        }
+
+    
+})}
 
 
 //debounce function because it was calling 2/3 times a click
@@ -121,7 +165,16 @@ function debounce(func, wait, immediate) {
     };
   };
 
+$('.bubble').on("click",(event) => {
+    mapSelection = event.currentTarget.id;
+    console.log(mapSelection);
 
+    map.removeLayer(Thunderforest);
+    Thunderforest = L.tileLayer(`https://tile.thunderforest.com/${mapSelection}/{z}/{x}/{y}.png?apikey=8c17817d10c0456c853764480540314f`, {
+	attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors', //attribution required
+	maxZoom: 22
+    }).addTo(map);
+})
 
 
 
